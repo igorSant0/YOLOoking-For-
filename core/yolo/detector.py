8ac47d93aed11ede484698fe419588363de322db
igorSant0 <igor705@hotmail.com>
2026-06-30 13:19:16 -0300

    modificando o modelo de identificação

--- core/yolo/detector.py
+++ core/yolo/detector.py
@@ -3,12 +3,14 @@ from ultralytics import YOLO
 
 from core.config.settings import permitted_classes
 
+
 class YoloDetector:
     def __init__(self):
-        self.model = YOLO("core/yolo/yolov8n.pt")
-        
+        self.model = YOLO("core/yolo/bestv26.pt")
+
         self.allowed_classes = [
-            class_id for class_id, class_name in self.model.names.items() 
+            class_id
+            for class_id, class_name in self.model.names.items()
             if class_name in permitted_classes
         ]
 
@@ -16,8 +18,10 @@ class YoloDetector:
         if frame is None:
             return None
 
-        results = self.model(frame, conf=0.5, classes=self.allowed_classes, verbose=False)
-        
+        results = self.model(
+            frame, conf=0.5, classes=self.allowed_classes, verbose=False
+        )
+
         annotated_frame = results[0].plot()
-        
+
         return annotated_frame
